ecdc7dc101938414c7fc8fc2b8c20494021b7087
Ovidiu Goanta <Goantza_jr@yahoo.com>
2022-01-06 13:20:08 +0100

    feat: add functionality for add-btn and remove-btn

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-const textInput = document.querySelector(".todo-list");
-const addBtn = document.querySelector(".add-btn");
-const removeBtn = document.querySelector(".remove-btn");
+const textInput = document.querySelector("#input-todo");
+const addBtn = document.querySelector("#add-btn");
+const removeBtn = document.querySelector("#remove-btn");
 const addedText = document.querySelector(".todo-text");
-const textToAdd = document.querySelector(".add-text");
+const allRadio = document.querySelector("#all-radio");
+const openRadio = document.querySelector("#open-radio");
+const doneRadio = document.querySelector("#done-radio");
 
-addBtn.addEventListener("click", () => {
-  addedText.innerHTML = textToAdd.value;
-  const newList = document.createElement("li");
-  textInput.classList.add("li");
-});
+function addNewList() {
+  addBtn.addEventListener("click", () => {
+    const newText = document.createElement("li");
+    const checkBox = document.createElement("input");
+    checkBox.setAttribute("type", "checkbox");
+    const newList = document.querySelector(".todo-list");
+
+    if (textInput.value !== " " && textInput.value.length >= 5) {
+      newText.innerHTML = textInput.value;
+      newList.appendChild(newText);
+      newText.appendChild(checkBox);
+    }
+    textInput.value = "";
+    removeBtn.addEventListener("click", () => {
+      if (checkBox.checked) {
+        newText.remove();
+        checkBox.remove();
+      }
+    });
+    checkBox.addEventListener("change", () => {
+      if (checkBox.checked) {
+        newText.style.textDecoration = "line-through";
+      } else {
+        newText.style.textDecoration = "";
+      }
+    });
+  });
+}
+addNewList();
